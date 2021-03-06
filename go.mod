8433d275e66bacd83b87c344c26b6a71c4b81b3f
Elvis <elvis@mapom.me>
2021-03-07 00:46:27 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/etombini/onsignal
+
+go 1.15
---
+++ onsignal.go
@@ -0,0 +1,61 @@
+package signal
+
+import (
+	"context"
+	"os"
+	"os/signal"
+)
+
+//Do allows to launch a function when a signal is caught.
+//If context is Done, signal handling stops.
+func Do(ctx context.Context, signals []os.Signal, f func()) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, signals...)
+
+	go func() {
+		defer signal.Stop(sigChan)
+		for {
+			select {
+			case <-sigChan:
+				f()
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+}
+
+//DoAndStop allows to call function when a signal is caught and then stop handling signals
+//If context is Done, signal handling stops.
+func DoAndStop(ctx context.Context, signals []os.Signal, f func()) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, signals...)
+
+	go func() {
+		defer signal.Stop(sigChan)
+		select {
+		case <-sigChan:
+			f()
+		case <-ctx.Done():
+			return
+		}
+	}()
+}
+
+//DoAndExit allows to call a function when a signal is caught and then exit the main program
+//If context is Done, signal handling stops.
+func DoAndExit(ctx context.Context, signals []os.Signal, f func(), exitCode int) {
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, signals...)
+
+	go func() {
+		defer signal.Stop(sigChan)
+		select {
+		case <-sigChan:
+			f()
+			os.Exit(exitCode)
+		case <-ctx.Done():
+			return
+		}
+	}()
+}
